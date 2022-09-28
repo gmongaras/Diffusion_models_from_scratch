Fixed some issues with scaling and predicting

src/helpers/PixelCNN_PP_helper_functions.py
@@ -59,17 +59,19 @@ def get_preds(mu, s_inv, pi, scaled):
     pix_vals = pix_vals.sum(-1)
     
     # Make the probabilities between 0 and 1
-    pix_vals /= pix_vals.sum(-1)
+    pix_vals /= torch.max(pix_vals.sum(-1), torch.tensor(1e-6))
     
     
     
     
-    ### Now that the probabilities are calculated, get the
-    ### argmax value of them to get the predicted pixel value
-    preds = []
-    for n in range(0, pix_vals.shape[0]):
-        preds.append(torch.multinomial(pix_vals[n], 1))
-    preds = torch.stack(preds)
+    ### Now that the probabilities are calculated, create a
+    ### multinomial distribution over all 256 pixel values
+    ### and sample the distribution
+    preds = torch.multinomial(pix_vals, 1)
+    
+    # If the values are scaled, scale the prediction
+    if scaled:
+        preds = ((preds - 127.5)/127.5)
     
     # Return the predictions
     return preds

src/helpers/PixelCNN_PP_loss.py
@@ -64,7 +64,8 @@ def PixelCNN_PP_loss(Y_hat, Y):
     
     # Calculate the loss for these values
     C1_im = (C1_X - C1_mu + scale)*C1_s_inv
-    C1_loss = C1_im - torch.nn.functional.softplus(C1_im)
+    # C1_loss = C1_im - torch.nn.functional.softplus(C1_im)
+    C1_loss = torch.log(torch.sigmoid(C1_im))
     
     # Scale the loss for each distribution and get the final
     # loss value
@@ -85,7 +86,8 @@ def PixelCNN_PP_loss(Y_hat, Y):
     
     # Calculate the loss for these values
     C2_im = (C2_X - C2_mu - scale)*C2_s_inv
-    C2_loss = -torch.nn.functional.softplus(C2_im)
+    # C2_loss = -torch.nn.functional.softplus(C2_im)
+    C2_loss = torch.log(1-torch.sigmoid(C2_im))
     
     # Scale the loss for each distribution and get the final
     # loss value