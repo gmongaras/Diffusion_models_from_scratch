Fixed variance optimization

src/main.py
@@ -59,7 +59,7 @@ def main():
     epochs = 10
     lr = 0.0001
     t_dim = 100
-    model = diff_model(inCh, embCh, chMult, num_heads, num_res_blocks, T, beta_sched, t_dim)
+    model = diff_model(inCh, embCh, chMult, num_heads, num_res_blocks, T, beta_sched, t_dim, device)
     
     # Load in a test image
     filePath = "./tests/testimg.gif"
@@ -72,14 +72,14 @@ def main():
     trainer.train(im)
     
     # What does a sample image look like?
-    noise = torch.randn_like(im[:1])
+    noise = torch.randn_like(im[:1]).to(model.device)
     for t in range(T, -1, -1):
         with torch.no_grad():
             noise = model.unnoise_batch(noise, t)
             
     # Convert the sample image to 0->255
     # and show it
-    noise = unreduce_image(noise)
+    noise = unreduce_image(noise).cpu().detach()
     plt.imshow(noise[0].permute(1, 2, 0))
     plt.savefig("fig.png")
     

src/model_trainer.py
@@ -94,6 +94,29 @@ class model_trainer():
         return loss
     
     
+    
+    # Loss for the variance
+    def loss_variance(self, v, t):
+        # Get the beta values for this batch of ts
+        beta_t, _, a_bar_t = self.model.get_scheduler_info(t)
+        
+        # Beta values for the previous value of t
+        _, _, a_bar_t1 = self.model.get_scheduler_info(t-1)
+        
+        # Get the beta tilde value
+        beta_tilde_t = ((1-a_bar_t1)/(1-a_bar_t))*beta_t
+        beta_tilde_t = self.model.unsqueeze(beta_tilde_t, -1, 3)
+        
+        # KL loss between the v values and t values
+        # Depending on the value of t, get the loss
+        loss = torch.where(t==0,
+                    -torch.log(v).flatten(1,-1).sum(-1).mean(),
+                    self.KL(v, beta_tilde_t).flatten(1,-1).sum(-1).mean()
+        ).mean()
+        
+        return loss
+    
+    
     # Combined loss
     # Inputs:
     #   epsilon - True epsilon values of shape (N, C, L, W)
@@ -108,12 +131,21 @@ class model_trainer():
         mean_t = self.model.noise_to_mean(epsilon_pred, x_t, t)
         var_t = self.model.vs_to_variance(v, t)
         
+        """
+        Note: The paper states that the loss for the
+        variance should be L_vlb, but this is not what they
+        use in their implementation. Instead, they use
+        the KL divergence between the predictions and
+        the Beta_t_tilde values
+        """
+        
         # Get the losses
         loss_simple = self.loss_simple(epsilon, epsilon_pred)
-        loss_vlb = self.loss_vlb(x_t, x_t1, mean_t, var_t, t)
+        # loss_vlb = self.loss_vlb(x_t, x_t1, mean_t, var_t, t)
+        loss_var = self.loss_variance(var_t, t)
         
         # Return the combined loss
-        return loss_simple + self.Lambda*loss_vlb
+        return loss_simple + self.Lambda*loss_var
         
     
     

src/models/diff_model.py
@@ -262,8 +262,11 @@ class diff_model(nn.Module):
             print(f"t values must either be a scalar, list of scalars, or a tensor of scalars, not type: {type(t)}")
             return
         
+        x_t = x_t.to(self.device)
+        t = t.to(self.device)
+        
         # Get the model predictions
-        noise_t, vs = self.forward(x_t, t)
+        noise_t, vs = self.forward(x_t.to(self.device), t)
         
         # Convert the noise to a mean and
         # the vs to variances
@@ -272,7 +275,7 @@ class diff_model(nn.Module):
         
         # Get the output of the predicted normal distribution
         # out = self.normal_dist(x_t, mean_t, var_t)
-        out = mean_t + torch.randn((mean_t.shape[0]))*torch.sqrt(var_t)
+        out = mean_t + torch.randn((mean_t.shape[0]), device=self.device)*torch.sqrt(var_t)
         
         # Return the image scaled to (0, 255)
         # return unreduce_image(out)