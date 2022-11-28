Fixed variance prediction

src/model_trainer.py
@@ -55,7 +55,7 @@ class model_trainer():
             
         # Uniform distribution for values of t
         self.t_vals = np.arange(0, self.T.detach().cpu().numpy())
-        self.T_dist = torch.distributions.uniform.Uniform(float(2.0)-float(0.499), float(self.T-1)+float(0.499))
+        self.T_dist = torch.distributions.uniform.Uniform(float(0.0)-float(0.499), float(self.T)+float(0.499))
         
         # Optimizer
         self.optim = torch.optim.AdamW(self.model.parameters(), lr=lr)
@@ -145,20 +145,33 @@ class model_trainer():
     # KL divergence between two gaussians
     # Formula derived from: https://stats.stackexchange.com/questions/7440/kl-divergence-between-two-univariate-gaussians
     # Inputs:
-    #   mean_real - The mean of the real distribution
-    #   mean_fake - Mean of the predicted distribution
-    #   std_real - Standard deviation of the real distribution
-    #   std_fake - Standard Deviation of the predicted distribution
+    #   t - Values of t of shape (N)
+    #   mean_real - The mean of the real distribution of shape (N, C, L, W)
+    #   mean_fake - Mean of the predicted distribution of shape (N, C, L, W)
+    #   var_real - Variance of the real distribution of shape (N, C, L, W)
+    #   var_fake - Variance of the predicted distribution of shape (N, C, L, W)
+    #   x_0 - The unoised image at time t = 0 of shape (N, C, L, W)
     # Outputs:
     #   Loss vector for each part of the entire batch
-    def loss_vlb_gauss(self, mean_real, mean_fake, std_real, std_fake):
+    def loss_vlb_gauss(self, t, mean_real, mean_fake, var_real, var_fake, x_0):
+        std_real = torch.sqrt(var_real)
+        std_fake = torch.sqrt(var_fake)
+
         # Note:
         # p (mean_real, std_real) - Distribution we want the model to predict
         # q (mean_fake, std_fake) - Distribution the model is predicting
-        return (torch.log(std_fake/std_real) \
-            + ((std_real**2) + (mean_real-mean_fake)**2)/(2*(std_fake**2)) \
+        output = (torch.log(std_fake/std_real) \
+            + ((var_real) + (mean_real-mean_fake)**2)/(2*(var_fake)) \
             - torch.tensor(1/2))\
             .flatten(1,-1).mean(-1)
+        
+        # Replace where t = 0
+        output = torch.where(t == 0,
+            -torch.distributions.Normal(mean_fake, std_fake).log_prob(x_0).flatten(1,-1).mean(-1),
+            output
+        )
+
+        return output
     
     
     # Combined loss
@@ -187,68 +200,62 @@ class model_trainer():
         """
 
         # Get the mean and variance from the model
-        mean_t_pred = self.model.noise_to_mean(epsilon_pred, x_t, t, True)
+        mean_t_pred = self.model.noise_to_mean(epsilon_pred, x_t, t, False)
         var_t_pred = self.model.vs_to_variance(v, t)
 
 
         ### Preparing for the real normal distribution
 
-        # Get the beta values for this batch of ts
-        beta_t, a_t, a_bar_t = self.model.get_scheduler_info(t)
-        
-        # Beta values for the previous value of t
-        _, _, a_bar_t1 = self.model.get_scheduler_info(t-1)
+        # Get the scheduler information
+        beta_t = self.model.scheduler.sample_beta_t(t)
+        a_bar_t = self.model.scheduler.sample_a_bar_t(t)
+        a_bar_t1 = self.model.scheduler.sample_a_bar_t1(t)
+        beta_tilde_t = self.model.scheduler.sample_beta_tilde_t(t)
+        sqrt_a_bar_t1 = self.model.scheduler.sample_sqrt_a_bar_t1(t)
+        sqrt_a_t = self.model.scheduler.sample_sqrt_a_t(t)
 
         # Unsqueezing the values to match shape
         beta_t = self.model.unsqueeze(beta_t, -1, 3)
-        a_t = self.model.unsqueeze(a_t, -1, 3)
         a_bar_t = self.model.unsqueeze(a_bar_t, -1, 3)
         a_bar_t1 = self.model.unsqueeze(a_bar_t1, -1, 3)
-
-        # Get the beta tilde value
-        beta_tilde_t = ((1-a_bar_t1)/(1-a_bar_t))*beta_t
+        beta_tilde_t = self.model.unsqueeze(beta_tilde_t, -1, 3)
+        sqrt_a_bar_t1 = self.model.unsqueeze(sqrt_a_bar_t1, -1, 3)
+        sqrt_a_t = self.model.unsqueeze(sqrt_a_t, -1, 3)
 
         # Get the true mean distribution
-        mean_t = ((torch.sqrt(a_bar_t1)*beta_t)/(1-a_bar_t))*x_0 +\
-            ((torch.sqrt(a_t)*(1-a_bar_t1))/(1-a_bar_t))*x_t
-
-        # Get the prior
-        q_x_t1 = self.model.normal_dist(x_t1, mean_t, beta_tilde_t) + 1e-10
+        mean_t = ((sqrt_a_bar_t1*beta_t)/(1-a_bar_t))*x_0 +\
+            ((sqrt_a_t*(1-a_bar_t1))/(1-a_bar_t))*x_t
         
         # Get the losses
         loss_simple = self.loss_simple(epsilon, epsilon_pred)
-        # loss_vlb = self.loss_vlb(x_t1, q_x_t1, mean_t_pred, var_t_pred, t)
-        loss_vlb = self.loss_vlb_gauss(mean_t, mean_t_pred.detach(), beta_tilde_t, var_t_pred)*self.Lambda
+        loss_vlb = self.loss_vlb_gauss(t, mean_t, mean_t_pred.detach(), beta_tilde_t, var_t_pred, x_0)*self.Lambda
 
         # Get the combined loss
         loss_comb = loss_simple + loss_vlb
-
-        # Reduce the losses
-        loss_simple = loss_simple.mean()
-        loss_vlb = loss_vlb.mean()
 
 
 
 
         # Update the loss storage for importance sampling
         if self.use_importance:
-            t = t.detach().cpu().numpy()
-            loss = loss_comb.detach().cpu()
-            self.update_losses(loss, t)
+            with torch.no_grad():
+                t = t.detach().cpu().numpy()
+                loss = loss_vlb.detach().cpu()
+                self.update_losses(loss, t)
 
-            # Have 10 loss values been sampled for each value of t?
-            if np.sum(self.losses_ct) == self.losses.size - 20:
-                # The losses are based on the probability for each
-                # value of t
-                p_t = np.sqrt((self.losses**2).mean(-1))
-                p_t = p_t / p_t.sum()
-                loss = loss / torch.tensor(p_t[t], device=loss.device)
-            # Otherwise, don't change the loss values
+                # Have 10 loss values been sampled for each value of t?
+                if np.sum(self.losses_ct) == self.losses.size - 20:
+                    # The losses are based on the probability for each
+                    # value of t
+                    p_t = np.sqrt((self.losses**2).mean(-1))
+                    p_t = p_t / p_t.sum()
+                    loss = loss / torch.tensor(p_t[t], device=loss.device)
+                # Otherwise, don't change the loss values
 
-        
-        
-        # Return the combined loss
-        return loss_comb.mean(), loss_simple, loss_vlb
+
+
+        # Return the losses
+        return loss_comb.mean(), loss_simple.mean(), loss_vlb.mean()
         
     
     
@@ -330,7 +337,7 @@ class model_trainer():
             self.losses_var.append(loss_var.item())
             self.epochs_list.append(epoch)
             
-            print(f"Loss at epoch #{epoch}  Combined: {round(loss.item(), 4)}    Mean: {round(loss_mean.item(), 4)}    Variance: {round(loss_var.item(), 4)}")
+            print(f"Loss at epoch #{epoch}  Combined: {round(loss.item(), 4)}    Mean: {round(loss_mean.item(), 4)}    Variance: {round(loss_var.item(), 6)}")
     
 
 

src/models/diff_model.py
@@ -7,6 +7,7 @@ import os
 import json
 import threading
 from ..blocks.convNext import convNext
+from .Variance_Scheduler import Variance_Scheduler
 
 
 
@@ -68,16 +69,8 @@ class diff_model(nn.Module):
         # U_net model
         self.unet = U_Net(inCh, inCh*2, embCh, chMult, t_dim, num_heads, num_res_blocks, useDeep, dropoutRate).to(device)
         
-        # What scheduler should be used to add noise
-        # to the data?
-        if self.beta_sched == "cosine":
-            def f(t):
-                s = 0.008
-                return torch.cos(((t/T + s)/(1+s)) * (torch.pi/2))**2 /\
-                    torch.cos(torch.tensor((s/(1+s)) * (torch.pi/2)))**2
-            self.beta_sched_funct = f
-        else: # Linear
-            self.beta_sched_funct = torch.linspace(1e-4, 0.02, T)
+        # Variance scheduler for values of beta and alpha
+        self.scheduler = Variance_Scheduler(beta_sched, T, self.device)
             
         # Used to embed the values of t so the model can use it
         self.t_emb = PositionalEncoding(t_dim).to(device)
@@ -90,42 +83,6 @@ class diff_model(nn.Module):
             
             
             
-    # Used to get the value of beta, a and a_bar from the schedulers
-    # Inputs:
-    #   t - Batch of t values of shape (N) 
-    #       Note: t values can be in the range [0, T-1]
-    # Outputs:
-    #   Batch of beta and a values:
-    #     beta_t
-    #     a_t
-    #     a_bar_t
-    def get_scheduler_info(self, t):
-        # Gradients don't matter here
-        with torch.no_grad():
-            # t value assertion
-            assert torch.all(t <= self.T-1) and torch.all(t >= -1), "The value of t can be in the range [-1, T-1]"
-            
-            # Values depend on the scheduler
-            if self.beta_sched == "cosine":
-                # Beta_t, a_t, and a_bar_t
-                # using the cosine scheduler
-                a_bar_t = self.beta_sched_funct(t)
-                a_bar_t1 = torch.where(t > 0, self.beta_sched_funct(t-1), a_bar_t)
-                beta_t = 1-(a_bar_t/(a_bar_t1))
-                beta_t = torch.clamp(beta_t, 0, 0.999)
-                a_t = 1-beta_t
-            else:
-                # Beta_t, a_t, and a_bar_t
-                # using the linear scheduler
-                beta_t = self.beta_sched_funct.repeat(t.shape[0])[t]
-                a_t = 1-beta_t
-                a_bar_t = torch.zeros(t.shape[0])
-                for b in range(0, t.shape[0]):
-                    a_bar_t[b] = torch.prod(1-self.beta_sched_funct[:t[b]+1])
-                
-            return beta_t.to(self.device), a_t.to(self.device), a_bar_t.to(self.device)
-    
-    
     # Unsqueezing n times along the given dim.
     # Note: dim can be 0 or -1
     def unsqueeze(self, X, dim, n):
@@ -144,18 +101,15 @@ class diff_model(nn.Module):
     #   Batch of noised images of shape (N, C, L, W)
     #   Noise added to the images of shape (N, C, L, W)
     def noise_batch(self, X, t):
-        # Make sure t isn't too large
-        t = torch.min(t, self.T)
-        
         # Sample gaussian noise
         epsilon = torch.randn_like(X, device=self.device)
         
         # The value of a_bar_t at timestep t depending on the scheduler
-        a_bar_t = self.unsqueeze(self.get_scheduler_info(t)[2], -1, 3)
-        t = self.unsqueeze(t, -1, 3)
+        sqrt_a_bar_t = self.unsqueeze(self.scheduler.sample_sqrt_a_bar_t(t), -1, 3)
+        sqrt_1_minus_a_bar_t = self.unsqueeze(self.scheduler.sample_sqrt_1_minus_a_bar_t(t), -1, 3)
         
         # Noise the images
-        return torch.sqrt(a_bar_t)*X + torch.sqrt(1-a_bar_t)*epsilon, epsilon
+        return sqrt_a_bar_t*X + sqrt_1_minus_a_bar_t*epsilon, epsilon
     
     
     
@@ -193,34 +147,42 @@ class diff_model(nn.Module):
 
 
         # Get the beta and a values for the batch of t values
-        beta_t, a_t, a_bar_t = self.get_scheduler_info(t)
-
-        # Get the previous a bar values for the batch of t-1 values
-        a_bar_t1 = torch.where(t == 0, 0, self.get_scheduler_info(t-1)[2])
+        beta_t = self.scheduler.sample_beta_t(t)
+        a_t = self.scheduler.sample_a_t(t)
+        sqrt_a_t = self.scheduler.sample_sqrt_a_t(t)
+        a_bar_t = self.scheduler.sample_a_bar_t(t)
+        sqrt_a_bar_t = self.scheduler.sample_sqrt_a_bar_t(t)
+        sqrt_1_minus_a_bar_t = self.scheduler.sample_sqrt_1_minus_a_bar_t(t)
+        a_bar_t1 = self.scheduler.sample_a_bar_t1(t)
+        sqrt_a_bar_t1 = self.scheduler.sample_sqrt_a_bar_t1(t)
 
         # Make sure everything is in the correct shape
         beta_t = self.unsqueeze(beta_t, -1, 3)
         a_t = self.unsqueeze(a_t, -1, 3)
+        sqrt_a_t = self.unsqueeze(sqrt_a_t, -1, 3)
         a_bar_t = self.unsqueeze(a_bar_t, -1, 3)
+        sqrt_a_bar_t = self.unsqueeze(sqrt_a_bar_t, -1, 3)
+        sqrt_1_minus_a_bar_t = self.unsqueeze(sqrt_1_minus_a_bar_t, -1, 3)
         a_bar_t1 = self.unsqueeze(a_bar_t1, -1, 3)
+        sqrt_a_bar_t1 = self.unsqueeze(sqrt_a_bar_t1, -1, 3)
         if len(t.shape) == 1:
             t = self.unsqueeze(t, -1, 3)
 
 
         # Calculate the uncorrected mean
         if corrected == False:
-            return (1/torch.sqrt(a_t))*(x_t - ((1-a_t)/torch.sqrt(1-a_bar_t))*epsilon)
+            return (1/sqrt_a_t)*(x_t - (beta_t/sqrt_1_minus_a_bar_t)*epsilon)
 
 
         # Calculate the corrected mean and return it
         mean = torch.where(t == 0,
             # When t is 0, normal without correction
-            (1/torch.sqrt(a_t))*(x_t - ((1-a_t)/torch.sqrt(1-a_bar_t))*epsilon),
+            (1/sqrt_a_t)*(x_t - (beta_t/sqrt_1_minus_a_bar_t)*epsilon),
 
             # When t is not 0, special with correction
-            (torch.sqrt(a_bar_t1)*beta_t)/(1-a_bar_t) * \
-                torch.clamp( (1/torch.sqrt(a_bar_t))*x_t - torch.sqrt((1-a_bar_t)/a_bar_t)*epsilon, -1, 1 ) + \
-                (((1-a_bar_t1)*torch.sqrt(a_t))/(1-a_bar_t))*x_t
+            (sqrt_a_bar_t1*beta_t)/(1-a_bar_t) * \
+                torch.clamp( (1/sqrt_a_bar_t)*x_t - (sqrt_1_minus_a_bar_t/sqrt_a_bar_t)*epsilon, -1, 1 ) + \
+                (((1-a_bar_t1)*sqrt_a_t)/(1-a_bar_t))*x_t
         )
         return mean
     
@@ -229,17 +191,10 @@ class diff_model(nn.Module):
     # Used to convert a batch of predicted v values to
     # a batch of variance predictions
     def vs_to_variance(self, v, t):
-        # Get the beta values for this batch of ts
-        beta_t, _, a_bar_t = self.get_scheduler_info(t)
-        
-        # Beta values for the previous value of t
-        _, _, a_bar_t1 = self.get_scheduler_info(t-1)
-        
-        # Get the beta tilde value
-        beta_tilde_t = ((1-a_bar_t1)/(1-a_bar_t))*beta_t
-        
-        beta_t = self.unsqueeze(beta_t, -1, 3)
-        beta_tilde_t = self.unsqueeze(beta_tilde_t, -1, 3)
+
+        # Get the scheduler information
+        beta_t = self.unsqueeze(self.scheduler.sample_beta_t(t), -1, 3)
+        beta_tilde_t = self.unsqueeze(self.scheduler.sample_beta_tilde_t(t), -1, 3)
 
         """
         Note: The authors claim that v stayed in the range of values
@@ -307,10 +262,13 @@ class diff_model(nn.Module):
     # Outputs:
     #   Distribution applied to x of the same shape as x
     def normal_dist(self, x, mean, var):
-        var = torch.where(torch.logical_and(var<1e-5, var>=0), var+1e-5, var)
-        var = torch.where(torch.logical_and(var>-1e-5, var<0), var-1e-5, var)
-        return (1/(var*torch.sqrt(torch.tensor(2*torch.pi))))*\
-            torch.exp((-1/2)*((x-mean)/var)**2)
+        std = torch.sqrt(var)
+
+        std = torch.where(torch.logical_and(std<1e-5, std>=0), std+1e-5, std)
+        std = torch.where(torch.logical_and(std>-1e-5, std<0), std-1e-5, std)
+        return (1/(std*torch.sqrt(torch.tensor(2*torch.pi))))\
+            * torch.exp((-1/2)*((x-mean)/std)**2) \
+            + 1e-10
     
     
     
@@ -348,15 +306,10 @@ class diff_model(nn.Module):
         noise_t, v_t = self.forward(x_t, t)
         
         # Convert the noise to a mean
-        mean_t = self.noise_to_mean(noise_t, x_t, t)
+        mean_t = self.noise_to_mean(noise_t, x_t, t, True)
 
         # Convert the v prediction variance
         var_t = self.vs_to_variance(v_t, t)
-        
-        # Get the beta t value
-        beta_t, _, _ = self.get_scheduler_info(t)
-        beta_t = self.unsqueeze(beta_t, -1, 3)
-        t = self.unsqueeze(t, -1, 3)
         
         # Get the output of the predicted normal distribution
         # out = self.normal_dist(x_t, mean_t, var_t)