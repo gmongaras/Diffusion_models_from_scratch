Fixed gen procedure from breaking and fixed FID calculation

src/main.py
@@ -41,8 +41,8 @@ def main():
     training = False
 
     ## Generation paramters (only in effect when generating samples, not during training)
-    step_size = 1                # Step size to take when generating images
-    DDIM_scale = 1          # Scale to transition between a DDIM, DDPM, or in between.
+    step_size = 100                # Step size to take when generating images
+    DDIM_scale = 0          # Scale to transition between a DDIM, DDPM, or in between.
                             # use 0 for pure DDIM and 1 for pure DDPM
     
     ## Saving params
@@ -122,13 +122,13 @@ def main():
         trainer.train(img_data)
     
     # What does a sample image look like?
-    noise, imgs = model.sample_imgs(1, True, True)
+    noise, imgs = model.sample_imgs(1, True, True, True)
             
     # Convert the sample image to 0->255
     # and show it
     plt.close('all')
     plt.axis('off')
-    noise = torch.clamp(unreduce_image(noise).cpu().detach().int(), 0, 255)
+    noise = torch.clamp(noise.cpu().detach().int(), 0, 255)
     for img in noise:
         plt.imshow(img.permute(1, 2, 0))
         plt.savefig("fig.png", bbox_inches='tight', pad_inches=0, )

src/models/diff_model.py
@@ -357,13 +357,19 @@ class diff_model(nn.Module):
         var_t = self.DDIM_scale*var_t
         beta_tilde_t = self.DDIM_scale*beta_tilde_t
 
+        # Sometimes the noise blows up. Since the noise prediction should follow a normal
+        # distribution, sample a normal distribution of the same shape and restrict the
+        # predicted noise to the min and max of that distribution.
+        samp = torch.randn_like(noise_t)
+        noise_t = noise_t.clamp(samp.min(), samp.max())
+
         # Get the x_0 and x_t_dir predictions. Note that the
         # x_t direction prediction uses the beta_tilde_t value
         # as this value makes the process a DDPM when the scale is 1
         # but if the predicted variance is used, this isn't necessarily true.
         # The predicted variance is used as in the improved DDPM paper
         x_0_pred = ((x_t-sqrt_1_minus_a_bar_t*noise_t)/sqrt_a_bar_t)
-        x_0_pred = x_0_pred.clamp(-1.5, 1.5)
+        # x_0_pred = x_0_pred.clamp(-1.5, 1.5)
         x_t_dir_pred = torch.sqrt(torch.clamp(1-a_bar_t1-beta_tilde_t, 0, torch.inf))*noise_t
         random_noise = torch.randn((noise_t.shape), device=self.device)*torch.sqrt(var_t)
 
@@ -388,7 +394,8 @@ class diff_model(nn.Module):
 
 
     # Sample a batch of generated samples from the model
-    def sample_imgs(self, batchSize, save_intermediate=False, use_tqdm=False):
+    @torch.no_grad()
+    def sample_imgs(self, batchSize, save_intermediate=False, use_tqdm=False, unreduce=False):
         # Make sure the model is in eval mode
         self.eval()
 
@@ -398,11 +405,13 @@ class diff_model(nn.Module):
         # Iterate T//step_size times to denoise the images
         imgs = []
         for t in tqdm(range(self.T, 0, -self.step_size)) if use_tqdm else range(self.T, 0, -self.step_size):
-            with torch.no_grad():
-                output = self.unnoise_batch(output, (t//self.step_size)-1)
-                if save_intermediate:
-                    imgs.append(unreduce_image(output[0]).cpu().detach().int().clamp(0, 255).permute(1, 2, 0))
+            output = self.unnoise_batch(output, (t//self.step_size)-1)
+            if save_intermediate:
+                imgs.append(unreduce_image(output[0]).cpu().detach().int().clamp(0, 255).permute(1, 2, 0))
         
+        if unreduce:
+            output = unreduce_image(output).clamp(0, 255)
+
         # Return the output images and potential intermediate output
         return (output,imgs) if save_intermediate else output
 