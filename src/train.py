094fdd95431d38f37d10d9b9160cc8277f455368
gmongaras <gabriel@mongaras.com>
2023-01-04 19:03:28 -0600

    Removed uneeded code in train script

--- src/train.py
+++ src/train.py
@@ -3,13 +3,8 @@ import pickle
 import torch
 from .models.diff_model import diff_model
 import numpy as np
-from PIL import Image
 from .model_trainer import model_trainer
-import matplotlib.pyplot as plt
-from .helpers.image_rescale import reduce_image, unreduce_image
 import math
-from tqdm import tqdm
-import matplotlib.animation as animation
 
 
 def train():
@@ -38,13 +33,6 @@ def train():
     p_uncond = 0.2          # Probability of training on a null class (only used if c_dim is not None)
     dropoutRate = 0.1
     use_importance = False # Should importance sampling be used to sample values of t?
-
-    training = True
-
-    ## Generation paramters (only in effect when generating samples, not during training)
-    step_size = 100                # Step size to take when generating images
-    DDIM_scale = 0          # Scale to transition between a DDIM, DDPM, or in between.
-                            # use 0 for pure DDIM and 1 for pure DDPM
     
     ## Saving params
     saveDir = "models/"
@@ -65,46 +53,45 @@ def train():
     
     ### Load in the data
     
-    if training:
-        # Open the zip files
-        archive1 = zipfile.ZipFile('data/Imagenet64_train_part1.zip', 'r')
-        archive2 = zipfile.ZipFile('data/Imagenet64_train_part2.zip', 'r')
-        
-        # Read the pickle data
-        data = []
-        labels = []
-        for filename in archive1.filelist:
-            file = pickle.load(archive1.open(filename.filename, "r"))
-            data.append(file["data"])
-            labels.append(file["labels"])
-            del file
-        for filename in archive2.filelist:
-            file = pickle.load(archive2.open(filename.filename, "r"))
-            data.append(file["data"])
-            labels.append(file["labels"])
-            del file
-        
-        # Load the data
-        archive1.close()
-        archive2.close()
-        img_data = np.concatenate((data), axis=0)
-        labels = np.concatenate((labels), axis=0)
-        del data
-
-        # Convert the data to a tensor
-        img_data = torch.tensor(img_data, dtype=torch.float32, device=torch.device("cpu"))
-        img_data = img_data.reshape(img_data.shape[0], 3, 64, 64)
+    # Open the zip files
+    archive1 = zipfile.ZipFile('data/Imagenet64_train_part1.zip', 'r')
+    archive2 = zipfile.ZipFile('data/Imagenet64_train_part2.zip', 'r')
     
+    # Read the pickle data
+    data = []
+    labels = []
+    for filename in archive1.filelist:
+        file = pickle.load(archive1.open(filename.filename, "r"))
+        data.append(file["data"])
+        labels.append(file["labels"])
+        del file
+    for filename in archive2.filelist:
+        file = pickle.load(archive2.open(filename.filename, "r"))
+        data.append(file["data"])
+        labels.append(file["labels"])
+        del file
+    
+    # Load the data
+    archive1.close()
+    archive2.close()
+    img_data = np.concatenate((data), axis=0)
+    labels = np.concatenate((labels), axis=0)
+    del data
+
+    # Convert the data to a tensor
+    img_data = torch.tensor(img_data, dtype=torch.float32, device=torch.device("cpu"))
+    img_data = img_data.reshape(img_data.shape[0], 3, 64, 64)
 
 
 
-        # Reshape the image to the nearest power of 2
-        if reshapeType == "down":
-            next_power_of_2 = 2**math.floor(math.log2(img_data.shape[-1]))
-        elif reshapeType == "up":
-            next_power_of_2 = 2**math.ceil(math.log2(img_data.shape[-1]))
-        if next_power_of_2 != img_data.shape[-1]:
-            img_data = torch.nn.functional.interpolate(img_data, (next_power_of_2, next_power_of_2))
+
+    # Reshape the image to the nearest power of 2
+    if reshapeType == "down":
+        next_power_of_2 = 2**math.floor(math.log2(img_data.shape[-1]))
+    elif reshapeType == "up":
+        next_power_of_2 = 2**math.ceil(math.log2(img_data.shape[-1]))
+    if next_power_of_2 != img_data.shape[-1]:
+        img_data = torch.nn.functional.interpolate(img_data, (next_power_of_2, next_power_of_2))
     
     # How many classes are there?
     num_classes = None
@@ -118,40 +105,15 @@ def train():
     
     
     ### Model Creation
-    model = diff_model(inCh, embCh, chMult, num_res_blocks, T, beta_sched, t_dim, device, c_dim, num_classes, dropoutRate, step_size if not training else 1, DDIM_scale)
+    model = diff_model(inCh, embCh, chMult, num_res_blocks, T, beta_sched, t_dim, device, c_dim, num_classes, dropoutRate)
     
     # Optional model loading
     if loadModel == True:
         model.loadModel(loadDir, loadFile, loadDefFile,)
     
     # Train the model
-    if training:
-        trainer = model_trainer(model, batchSize, numSteps, epochs, lr, device, Lambda, saveDir, numSaveEpochs, use_importance, p_uncond)
-        trainer.train(img_data, labels if c_dim != None else None)
-    
-    # What does a sample image look like?
-    noise, imgs = model.sample_imgs(1, True, True, True)
-            
-    # Convert the sample image to 0->255
-    # and show it
-    plt.close('all')
-    plt.axis('off')
-    noise = torch.clamp(noise.cpu().detach().int(), 0, 255)
-    for img in noise:
-        plt.imshow(img.permute(1, 2, 0))
-        plt.savefig("fig.png", bbox_inches='tight', pad_inches=0, )
-        plt.show()
-
-    # Image evolution gif
-    plt.close('all')
-    fig, ax = plt.subplots()
-    ax.set_axis_off()
-    for i in range(0, len(imgs)):
-        title = plt.text(imgs[i].shape[0]//2, -5, f"t = {i}", ha='center')
-        imgs[i] = [plt.imshow(imgs[i], animated=True), title]
-    animate = animation.ArtistAnimation(fig, imgs, interval=1, blit=True, repeat_delay=1000)
-    animate.save('diffusion.gif', writer=animation.PillowWriter(fps=50))
-    # plt.show()
+    trainer = model_trainer(model, batchSize, numSteps, epochs, lr, device, Lambda, saveDir, numSaveEpochs, use_importance, p_uncond)
+    trainer.train(img_data, labels if c_dim != None else None)
     
     
     
