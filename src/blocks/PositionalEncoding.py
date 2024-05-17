f2e76317d70eb565a953f4959f5781a010177318
Gabriel Mongaras <43501738+gmongaras@users.noreply.github.com>
2024-05-17 09:39:44 -0500

    Error message in PositionalEncoding.py

--- src/blocks/PositionalEncoding.py
+++ src/blocks/PositionalEncoding.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from colorama import Fore
 
 
 
@@ -14,6 +15,12 @@ class PositionalEncoding(nn.Module):
         self.denom = torch.tensor(10000)**\
             ((2*torch.arange(self.dim))/self.dim)
 
+
+        print(Fore.RED + '******************************************************************************************************************')
+        print(Fore.RED + '*If loading a pretrained model, uncomment lines 38/39 and comment lines 34/35 in src/blocks/PositionalEncoding.py*')
+        print(Fore.RED + '*Error with indices in the initial implementaion, applying PEs on the batch.                                     *')
+        print(Fore.RED + '******************************************************************************************************************')
+
     # Convert time steps to embedding tensors
     # Inputs:
     #   time - Time values of shape (N)
@@ -24,7 +31,12 @@ class PositionalEncoding(nn.Module):
         embeddings = time[:, None]*self.denom[None, :].to(time.device)
 
         # Sin/Cos transformation for even, odd indices
-        embeddings[::2] = embeddings[::2].sin()
-        embeddings[1::2] = embeddings[1::2].cos()
+        embeddings[:, ::2] = embeddings[:, ::2].sin()
+        embeddings[:, 1::2] = embeddings[:, 1::2].cos()
+
+        # # Sin/Cos transformation for even, odd indices
+        # embeddings[::2] = embeddings[::2].sin()
+        # embeddings[1::2] = embeddings[1::2].cos()
+        # Uncomment these ^ if loading a pretrained model
 
         return embeddings
